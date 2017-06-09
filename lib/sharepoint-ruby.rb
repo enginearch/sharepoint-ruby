c65197239ed94305f33f5060109eaf50f859e592
Andrii Votiakov <andrii@enginearch.com>
2017-06-09 10:51:36 +0200

    wip

--- lib/sharepoint-ruby.rb
+++ lib/sharepoint-ruby.rb
@@ -81,8 +81,10 @@ module Sharepoint
         block.call curl           unless block.nil?
       end
 
-      unless skip_json || (result.body_str.nil? || result.body_str.empty?)
+      puts "\n\n\n\n\n\n\n\n\n\n CONDITIONS :: #{skip_json}  #{result.body_str.nil?}   #{result.body_str.empty?}"
+      if !skip_json || !(result.body_str.nil? || result.body_str.empty?)
         begin
+          puts "\n\n\n\n\n CURL RESULT DATA BEFORE PARSE :::: #{result.body_str.inspect} \n\n\n\n\n\n"
           data = JSON.parse result.body_str
           puts "\n\n\n\n\n CURL RESPONSE DATA :::: #{data.inspect} \n\n\n\n\n\n"
           raise Sharepoint::SPException.new data, uri, body unless data['error'].nil?
